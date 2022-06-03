a13fb2c6376383cd8a10cc5da81b4a6ac73fb106
Emmaculine <veraemmaculine254@gmail.com>
2022-06-02 18:32:10 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 /**
  * main - my entry point
  * Description: prints "Programming is like bulding a multilingual puzzle"
- * Return: O
- */
+ * Return: 0
+*/
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return (O);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
