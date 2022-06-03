0bd96fc568a22c5357f7967cdb287f1b5d2243d9
Emmaculine <veraemmaculine254@gmail.com>
2022-06-02 19:01:28 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - my entry point
+ * Description: prints all sizes in c
+ * Return:0
+ */
+int main(void)
+{
+char a;
+int b;
+int long c;
+int long long d;
+printf("size of a char: %zu byte(s)\n", sizeof(a));
+printf("size of an int: %zu byte(s)\n", sizeof(b));
+printf("size of a long int: %zu byte(s)\n", sizeof(c));
+printf("size of a long long int: %zu byte(s)\n", sizeof(d));
+printf("size of a float: %zubyte(s)\n", sizeof(float));
+return (0);
+}
