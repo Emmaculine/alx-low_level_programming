1d6a34c41774b5b9c2290e901afacd9f47515050
Emmaculine <veraemmaculine254@gmail.com>
2022-06-02 19:42:52 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - my entry point 
+ * main - my entry point
  * Description: 'and that piece of art is useful" - Dora Korpar, 2015-10-19'
  * Return: 1
  */
